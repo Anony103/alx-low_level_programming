73bbb82a38479ad42ab53fa35f66cac3721327de
Abdulfatai <anonymakon@gmail.com>
2023-08-07 16:12:59 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (tect_content)
+	if (text_content)
 	{
 
 		for (size = 0; text_content[size] != '\0'; size++);
@@ -29,4 +29,3 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
-
