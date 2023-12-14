e3d8703fcb64b950f36f989185e1d4ac7780d984
Abdulfatai <anonymakon@gmail.com>
2023-12-14 10:25:52 -0800

    first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0;
-	unsigned int i = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
-		return 0;
+	if (!b)
+		return (0);
 
-	while (b[i] != '\0')
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return 0;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-		dec <<= 1;
-		if (b[i] == '1')
-			dec += 1;
-
-		i++;
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	return dec;
+	return (ui);
 }
